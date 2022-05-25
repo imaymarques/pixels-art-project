77576a3e2481ef65d2df424f4c335919dd101816
Mayara Marques <mayaramarquescosta@outlook.com>
2022-05-25 18:22:57 -0300

    Adicionando o requisito 4 do projeto

---
+++ script.js
@@ -0,0 +1,6 @@
+// const getId = document.getElementById('pixel-board');
+// for (let index = 0; index <= 25; index += 1) {
+//     let newDiv = document.createElement('div');
+//     getId.appendChild(newDiv);
+//     newDiv.className = "pixel";
+// }
