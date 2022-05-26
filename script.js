776d119262d3b37e73ba377564a63ca84e01a110
Mayara Marques <mayaramarquescosta@outlook.com>
2022-05-26 19:49:03 -0300

    Adicionando o requisito 8 do projeto

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ let palet = document.querySelectorAll('.color');
 for (let index = 0; index < palet.length; index += 1) {
   palet[index].addEventListener('click', changeClass);
 }
+
+function changeColor(event) {
+  let color = document.querySelector('.selected');
+  let background = window.getComputedStyle(color).getPropertyValue('background-color');
+  event.target.style.background = background;
+}
+
+function click() {
+  let board = document.getElementById('pixel-board');
+  board.addEventListener('click', changeColor);
+}
+click();
